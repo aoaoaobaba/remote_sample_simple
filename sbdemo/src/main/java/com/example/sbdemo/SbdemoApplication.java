480dfdc6e8feec64549065361377f0f0d1257f9b
aoba <blueleaf1412.3@gmail.com>
2023-12-15 03:47:32 +0000

    処理を移動

--- sbdemo/src/main/java/com/example/sbdemo/SbdemoApplication.java
+++ sbdemo/src/main/java/com/example/sbdemo/SbdemoApplication.java
@@ -2,9 +2,17 @@ package com.example.sbdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SbdemoApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Welcome to Spring Boot!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbdemoApplication.class, args);
